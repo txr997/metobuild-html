17b92b06fad82c094ea36aa8f5c626df1f0de52d
akmol hosen <akmolhosen997@gmail.com>
2026-07-04 18:56:26 +0600

    h1-80%

--- assets/js/main.js
+++ assets/js/main.js
@@ -305,209 +305,39 @@ var mb_services1_slider = new Swiper(".mb_services1_slider", {
 });
 
 
-// parallax-images
-if ($(".wa_magnetic_1_trigger").length) {
-    var waMagnets2v2 = document.querySelectorAll('.wa_magnetic_1_trigger');
-    var waStrength2v2 = 30;
 
-    waMagnets2v2.forEach((magnet) => {
-        magnet.addEventListener('mousemove', moveMagnet2);
-        magnet.addEventListener('mouseout', function(event) {
-            const innerElements = event.currentTarget.querySelectorAll('.wa_magnetic_1_elm');
-            innerElements.forEach((elm) => {
-                gsap.to(elm, {
-                    x: 0,
-                    y: 0,
-					scale: 1.05,
-                    duration: 1,
-                    ease: "ease1"
-                });
-            });
-        });
-    });
-
-    function moveMagnet2(event) {
-        var magnetButton = event.currentTarget;
-        var bounding = magnetButton.getBoundingClientRect();
-        const innerElements = magnetButton.querySelectorAll('.wa_magnetic_1_elm');
-
-        const xMove = (((event.clientX - bounding.left) / magnetButton.offsetWidth) - 0.5) * waStrength2v2;
-        const yMove = (((event.clientY - bounding.top) / magnetButton.offsetHeight) - 0.5) * waStrength2v2;
-
-        innerElements.forEach((elm) => {
-            gsap.to(elm, {
-                x: xMove,
-                y: yMove,
-				scale: 1.05,
-                duration: 1,
-                ease: "ease1"
-            });
-        });
-    }
-}
-
-// clients-1-slider
-var el_clients1_slider = new Swiper(".el_clients1_slider", {
+// projects-1-slider
+var mb_project1_slider = new Swiper(".mb_project1_slider", {
 	loop: true,
 	speed: 800,
-	spaceBetween: 70,
-	autoplay: { delay: 4000 },
+	autoplay: { delay: 5000 },
+	effect: "fade",
+	fadeEffect: {
+		crossFade: true,
+	},
 
-	breakpoints: {
-		320: {
-		  slidesPerView: 2,
-		},
-		768: {
-		  slidesPerView: 3,
-		},
-		992: {
-		  slidesPerView: 4,
-		},
-		1200: {
-		  slidesPerView: 6,
-		}
-	}
+
+	pagination: {
+        el: ".mb_project1_slider_pagination",
+        clickable: true, 
+    },
+
 });
 
-
-
-
-// about-1-card-animation
-if (window.matchMedia("(min-width: 1200px)").matches) { 
-	const about1tl = gsap.timeline({
-		scrollTrigger: {
-		  trigger: ".ag-about-1-card", 
-		  start: "top 50%", 
-		  toggleActions: "play none none reverse", 
-		  markers: false 
-		}
-	  });
-	
-	  about1tl.from(".ag-about-1-card .has-ani:nth-of-type(1)", { 
-		yPercent: 100,
-		duration: .5
-	  })
-	
-	  about1tl.from(".ag-about-1-card .has-ani:nth-of-type(2)", { 
-		yPercent: -100,
-		duration: .5
-	  },"<")
-}
-
-
-// footer-2-big-title
-if($(".el-footer-2-big-title").length) {
-	function initElFooterTitleHover(selector) {
-		const titleEl = document.querySelector(selector);
-		if (!titleEl) return;
-	
-		const text = titleEl.textContent.trim();
-		titleEl.innerHTML = text
-		.split('')
-		.map((char) => `<span class="el-letter">${char === ' ' ? '&nbsp;' : char}</span>`)
-		.join('');
-	
-		const letters = titleEl.querySelectorAll('.el-letter');
-	
-		const yOffsets = [-50, -30, -15, -0,];
-	
-		let letterCenters = [];
-	
-		function cacheLetterPositions() {
-		letterCenters = Array.from(letters).map((letter) => {
-			const rect = letter.getBoundingClientRect();
-			return rect.left + rect.width / 2;
-		});
-		}
-	
-		cacheLetterPositions();
-		window.addEventListener('resize', cacheLetterPositions);
-	
-		titleEl.addEventListener('mousemove', (e) => {
-		const mouseX = e.clientX;
-		let activeIndex = 0;
-		let minDist = Infinity;
-	
-		letterCenters.forEach((centerX, i) => {
-			const dist = Math.abs(mouseX - centerX);
-			if (dist < minDist) {
-			minDist = dist;
-			activeIndex = i;
-			}
-		});
-	
-		letters.forEach((letter, i) => {
-			const distance = Math.abs(i - activeIndex);
-			const yValue = distance < yOffsets.length ? yOffsets[distance] : 0;
-	
-			gsap.to(letter, {
-			y: yValue,
-			duration: 0.5,
-			ease: 'power1.out',
-			overwrite: 'auto',
-			});
-		});
-		});
-	
-		titleEl.addEventListener('mouseleave', () => {
-		gsap.to(letters, {
-			y: 0,
-			duration: 0.6,
-			ease: 'power1.out',
-			overwrite: 'auto',
-		});
-		});
-	}
-	
-	document.addEventListener('DOMContentLoaded', () => {
-		initElFooterTitleHover('.el-footer-2-big-title');
-	});
-}
-
-
-
-// projects-3-slider
-var el_p3_slider = new Swiper(".el_p3_slider", {
+// testimonial-1-slider
+var mb_testimonial1_slider = new Swiper(".mb_testimonial1_slider", {
 	loop: true,
 	speed: 800,
-	spaceBetween: 24,
-	slidesPerView: 3,
+	spaceBetween: 28,
+	slidesPerView: "auto",
+	// autoplay: { delay: 4000 },
 	centeredSlides: true,
     roundLengths: true,
 
-	// autoplay: { delay: 5000 },
-
 	navigation: {
-		nextEl: ".el_p3_slider_next",
-		prevEl: ".el_p3_slider_prev",
+		nextEl: '.mb_testimonial1_slider_next',
+		prevEl: '.mb_testimonial1_slider_prev',
 	},
-	pagination: {
-        el: ".el_p3_slider_pagination",
-        type: "fraction",
-    },
-	scrollbar: {
-        el: ".el_p3_slider_fraction",
-        hide: true,
-    },
-
-	breakpoints: {
-		0: {
-			slidesPerView: 1,
-		},
-
-		1400: {
-			slidesPerView: 2,
-		},
-
-		1600: {
-			slidesPerView: 2,
-		},
-
-		1800: {
-			slidesPerView: 3,
-		},
-	}
-	
 });
 
 
